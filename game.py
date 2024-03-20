d1a12ffb47578d94057414fcc019d291fbc14f74
supreet-singh545 <ssupreet545>
2024-03-20 22:56:55 +0000

    Added loop and play again functionality

--- game.py
+++ game.py
@@ -12,9 +12,18 @@ if user_choice == computer_choice:
 elif (user_choice == "rock" and computer_choice == "scissors") 
      (user_choice == "paper" and computer_choice == "rock") 
      (user_choice == "scissors" and computer_choice == "paper"):
+
     print("You win!")
 else:
     print("Computer wins!")
 
 print(f"Computer chose: {computer_choice}")
+play_again = True
+while play_again:
+    
+
+    
+    play_again_input = input("Do you want to play again? (yes/no): ").lower()
+    if play_again_input != "yes":
+        play_again = False
 
